6f2617ce535bd6aa24bf382cc877b97ed4590a4c
Viktoria <viktoriapredko1@gmail.com>
2022-09-09 15:20:55 +0300

    Solve task4

--- homework/lec.py
+++ homework/lec.py
@@ -29,3 +29,9 @@
 #     sum = sum + result
 #     i=i+1
 # print(round(sum))
+
+# 4.Реализуйте алгоритм перемешивания списка
+import random
+list = [1, 2, 3, 4, 5, 6]
+random.shuffle(list)
+print(list)
